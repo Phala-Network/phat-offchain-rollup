a9e415be2d2a1616e134d31ca7f597be0ee77b29
h4x3rotab <h4x3rotab@gmail.com>
2023-04-14 10:27:58 +0000

    evm: remove unused files

--- evm/contracts/PhatRollupReceiver.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.9;
-
-abstract contract PhatRollupReceiver {
-    // bytes4(keccak256("onPhatRollupReceived(address,bytes)"))
-    bytes4 constant ROLLUP_RECEIVED = 0x43a53d89;
-    function onPhatRollupReceived(address _from, bytes calldata _action)
-        public virtual returns(bytes4);
-}
