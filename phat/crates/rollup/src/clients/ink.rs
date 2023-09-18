56b09c634199032a04ebb673faa8e26ccfb1ed91
GuiGou <guigoucrypto@gmail.com>
2023-09-18 17:36:57 +0200

    Merge the traits KvStore, MessageQueue and RollupAnchor for simplification

--- phat/crates/rollup/src/clients/ink.rs
+++ phat/crates/rollup/src/clients/ink.rs
@@ -60,7 +60,7 @@ impl<'a> KvSnapshot for InkSnapshot<'a> {
         let value: QueryResult = contract
             .query_at(
                 *self.contract_id,
-                ink::selector_bytes!("KvStore::get_value"),
+                ink::selector_bytes!("RollupAnchor::get_value"),
                 Some(&key),
                 0,
                 Some(self.at),
